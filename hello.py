c4c6f5f942597332f2df7def6286d2ef13eeb954
Bladerunner-K <121509754+Bladerunner-K@users.noreply.github.com>
2023-02-07 11:18:04 +0000

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello world")
+import fire
+
+def greeting(name):
+	return "Hello " + name
+
+fire.Fire(greeting("World!"))
+
+
+operator = "flash"
+
+
+if (operator >= "flash"):
+    print("this is the correct person")
+else:
+    print("this is the wrong person")
+
+
